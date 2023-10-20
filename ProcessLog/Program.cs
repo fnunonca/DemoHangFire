cb8d4e5637f153f165852d1d4074800afece6aef
Fernando Vasquez Nuñonca <MC1767@izipay.pe>
2023-10-20 09:55:55 -0500

    v2

--- ProcessLog/Program.cs
+++ ProcessLog/Program.cs
@@ -10,21 +10,24 @@ namespace LogAnalyzer
         private const string RootDirectory = @"D:\Log\TARJETAW";
         private const string SearchPattern = "nlog-apicontrollerpayments-*.log";
         private const string LogPattern = @"\|Payment\|Notification\| Status Code Response 404";
+        private const string LogEndPattern = @"\|Payment\|End\|response:";
+        private const string OutputFile = @"D:\OutputLog.txt"; // ruta donde se guardará el archivo
 
         static void Main(string[] args)
         {
             var logFiles = GetLogFiles(RootDirectory, SearchPattern);
 
+            var allResponses = new List<string>();
+
             foreach (var logFile in logFiles)
             {
-                var transactionIds = ExtractTransactionIds(logFile, LogPattern);
-                foreach (var id in transactionIds)
-                {
-                    Console.WriteLine($"Transaction ID from {logFile}: {id}");
-                }
+                var responses = ExtractResponseLines(logFile, LogEndPattern);
+                allResponses.AddRange(responses);
             }
 
-            Console.WriteLine("Finished processing. Press any key to exit.");
+            File.WriteAllLines(OutputFile, allResponses);
+
+            Console.WriteLine($"Finished processing. Responses written to {OutputFile}. Press any key to exit.");
             Console.ReadKey();
         }
 
@@ -33,24 +36,20 @@ namespace LogAnalyzer
             return Directory.GetFiles(rootDirectory, searchPattern, SearchOption.AllDirectories).ToList();
         }
 
-        private static IEnumerable<string> ExtractTransactionIds(string filePath, string pattern)
+        private static IEnumerable<string> ExtractResponseLines(string filePath, string endPattern)
         {
-            var transactionIds = new List<string>();
+            var responseLines = new List<string>();
             var lines = File.ReadAllLines(filePath);
 
             foreach (var line in lines)
             {
-                if (Regex.IsMatch(line, pattern))
+                if (Regex.IsMatch(line, endPattern))
                 {
-                    var parts = line.Split('|');
-                    if (parts.Length >= 4)
-                    {
-                        transactionIds.Add(parts[3]);
-                    }
+                    responseLines.Add(line);
                 }
             }
 
-            return transactionIds;
+            return responseLines;
         }
     }
 }
